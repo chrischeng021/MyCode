aba6d2384cf705afa82ded3e0e84ad1f4e7eebaf
Yexin Cheng <yexcheng@microsoft.com>
2020-06-20 23:50:09 +0800

    Resolve all easy module LinkedList problems.

--- com/leetcode/Solution.java
+++ com/leetcode/Solution.java
@@ -3,11 +3,15 @@ package com.leetcode;
 import java.util.Arrays;
 
 import com.leetcode.easy.DailyTemperature;
+import com.leetcode.easy.LinkedListUtil;
+import com.leetcode.model.ListNode;
 
-public class Solution
-{
-    public static void main(final String[] args)
-    {
-        System.out.println(Arrays.toString(DailyTemperature.dailyTemperatures(new int[]{97,32,43,78})));
+public class Solution {
+    public static void main(final String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+        System.out.println(LinkedListUtil.isPalindrome(head));
     }
 }
