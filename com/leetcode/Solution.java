4646e5b01e7a9622f8774b2d9237187a0919eacc
chrischeng021 <chrischeng021@gmail.com>
2020-07-06 00:56:10 +0800

    Submit Mac change

--- com/leetcode/Solution.java
+++ com/leetcode/Solution.java
@@ -1,21 +1,26 @@
 package com.leetcode;
 
-import com.leetcode.easy.ArrayUtils;
+import java.util.List;
+
+import com.leetcode.ArrayUtils;
 import com.leetcode.middle.KthMinValueInMatrix;
 
 public class Solution {
-    public static int hammingWeight(long n) {
-        int count = 0;
-        while(n > 0){
-            if(n % 2 == 1){
-                count++;
-            }
-            n = n >> 1;
+    public static void printArray(int[] arr){
+        for(int num : arr){
+            System.out.print(num + " ");
         }
-        return count;
+        System.out.println();
+    }
+
+    public static void printList(List<Integer> list){
+        for(int num : list){
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
     
     public static void main(String[] args) {
-        System.out.println(new KthMinValueInMatrix().kthSmallest(new int[][]{{1, 5, 9}, {10, 11, 13}, {12, 13, 15}}, 8));
+        System.out.println(ArrayUtils.countLargestGroup(12));
     }
 }
